b2b693569aafb8765d5777ead2f7eae433e1835b
Backwards <AlexHardingYT@gmail.com>
2024-01-23 14:41:02 -0500

    Added Comments
    Added Comments for clarity and for a better understanding of the code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// Import Node Packages
 import http from "node:http";
 import express from "express";
 import {route, dev_route} from "./types";
@@ -6,124 +7,173 @@ import httpProxy from "http-proxy";
 import {readFileSync, readdirSync} from "node:fs";
 import chalk from "chalk";
 
-const app = express();
-const proxy = httpProxy.createProxyServer();
+// Initiate Packages
+const app = express(); // Create Express Application
+const proxy = httpProxy.createProxyServer(); // Create Proxy Server
 
-const routes: route[] = JSON.parse(readFileSync(join(__dirname, "routes", "routes.json")).toString());
-const dev_routes: dev_route[] = JSON.parse(readFileSync(join(__dirname, "routes", "dev-routes.json")).toString())
+// Constants
+const routes: route[] = JSON.parse(readFileSync(join(__dirname, "routes", "routes.json")).toString()); // Path to Routes file
+const dev_routes: dev_route[] = JSON.parse(readFileSync(join(__dirname, "routes", "dev-routes.json")).toString()) // Path to Development Routes file
 
+/**
+ * Checks a given IP is active by probing it with a get request,
+ * looking for the status code 200.
+ * @param ip The IP to check.
+ * @returns Boolean, whether the ip returns 200
+ */
 function getServerRes(ip: string) {
-	return new Promise((res) => {
-		http.get(ip, (response) => {
-			const {statusCode} = response;
-			if (statusCode == 200) return res(true)
-			else return res(false);
-		}).on("error", (err) => {
-			return res(false);
+	return new Promise((res) => { // Create new Promise
+		http.get(ip, (response) => { // Send the HTTP request to the given IP
+			const {statusCode} = response; // pull the Status Code from the response
+			if (statusCode == 200) return res(true) // if the Status Code is 200, return true
+			else return res(false); // else return false.
+		}).on("error", (err) => { // If the HTTP request fails,
+			return res(false); // default return false
 		});
 	});
 }
 
+/**
+ * Adds "http://" to the beginning of the required string,
+ * this turns it into a valid URL as it is required by certain functions.
+ * @param url The URL to "httpify"
+ * @returns the hostname with http
+ */
 function httpify(url: string): string {
-    if (url.startsWith("http")) return url
-    else return `http://${url}`;
+    if (url.startsWith("http")) return url // Return the url as-is if it already has the "http://" prefix
+    else return `http://${url}`; // otherwise add the "http://" prefix and return it.
 }
 
-function findOverlap(a: any[], b: any[]) {
-    const out: any[] = [];
-    a.forEach(item => {
-        if (b.includes(item)) {
-            out.push(item);
+/**
+ * Takes two arrays and compares them for any overlapping/similar items.
+ * Will then return a list of all the similar items.
+ * @param a First Array
+ * @param b Second Array
+ * @returns Array of Overlaps
+ */
+function findOverlap(a: any[], b: any[]): any[] {
+    const out: any[] = []; // Create new "output" array
+    a.forEach(item => { // For every item in the first array,
+        if (b.includes(item)) { // if the item is in the second array,
+            out.push(item); // add the item to output.
         }
     });
-    return out;
+    return out; // return the output.
 }
 
+/**
+ * The Main function, finds if hostname is in routes file, will proxy the webserver if it exists and its up.
+ * @param connectionReq Hostname
+ * @param req Express Request
+ * @param res Express Response
+ */
 async function main(connectionReq: any, req: any, res: any) {
-    const end_filter = routes.filter(route => route.url.endsWith(connectionReq));
-    const start_filter = routes.filter(route => route.url.startsWith(connectionReq));
-    const urls = findOverlap(end_filter, start_filter);
+    const end_filter = routes.filter(route => route.url.endsWith(connectionReq)); // Filter Routes by comparing the Hostname to the end of the route
+    const start_filter = routes.filter(route => route.url.startsWith(connectionReq)); // Filter Routes by comparing the Hostname to the beginning of the route
+    const urls = findOverlap(end_filter, start_filter); // Find the Overlaps between the start and end filter
+    
+    // Switch Statement checking the number of overlapping items.
     switch (urls.length) {
         case 0:
-            res.sendStatus(404);
-            console.warn(chalk.yellow(`No route setup for: ${connectionReq}.`));
-            break;
+            res.sendStatus(404); // Send Status 404 if there is no overlap
+            console.warn(chalk.yellow(`No route setup for: ${connectionReq}.`)); // Log Warning
+            break; // End Request
         case 1:
+            // Check Server Status.
             if (await getServerRes(httpify(urls[0].route)) === false) {
-                res.sendStatus(503);
-                console.warn(chalk.red(`Cannot reach route: ${urls[0].route}.`));
-                break;
+                res.sendStatus(503); // Send Status 503 if server is down.
+                console.warn(chalk.red(`Cannot reach route: ${urls[0].route}.`)); // Log Error
+                break; // End Request
             }
-            proxy.web(req, res, { target: httpify(urls[0].route) });
-            break;
+            proxy.web(req, res, { target: httpify(urls[0].route) }); // Proxy WebServer
+            break; // End Request
         default:
-            res.sendStatus(500);
-            console.warn(chalk.red(`Multiple routes for: ${connectionReq}.\nAll Routes:\n${urls}`));
-            break;
+            res.sendStatus(500); // Send Status 500 if there are multiple overlapping routes.
+            console.warn(chalk.red(`Multiple routes for: ${connectionReq}.\nAll Routes:\n${urls}`)); // Log Warning
+            break; // End Request
     }
 }
 
+/**
+ * Like the Main function, finds if hostname is in routes file, will run the development file if it exists.
+ * @param connectionReq Hostname
+ * @param req Express Request
+ * @param res Express Response
+ */
 async function development_route(connectionReq: any, req: any, res: any) {
-    const end_filter = dev_routes.filter(route => route.url.endsWith(connectionReq));
-    const start_filter = dev_routes.filter(route => route.url.startsWith(connectionReq));
-    const urls: dev_route[] = findOverlap(end_filter, start_filter);
+    const end_filter = dev_routes.filter(route => route.url.endsWith(connectionReq)); // Filter Routes by comparing the Hostname to the end of the route
+    const start_filter = dev_routes.filter(route => route.url.startsWith(connectionReq)); // Filter Routes by comparing the Hostname to the beginning of the route
+    const urls: dev_route[] = findOverlap(end_filter, start_filter); // Find the Overlaps between the start and end filter
+
+    // Switch Statement checking the number of overlapping items.
     switch (urls.length) {
         case 0:
-            res.sendStatus(404);
-            console.warn(chalk.yellow(`No route setup for: ${connectionReq}.`));
-            break;
+            res.sendStatus(404); // Send Status 404 if there is no overlap
+            console.warn(chalk.yellow(`No route setup for: ${connectionReq}.`)); // Log Warning
+            break; // End Request.
         case 1:
-            const url = urls[0];
-            console.log(`Got Dev Request from: ${url.url}.`)
-            const scripts_path = join(__dirname, "development");
+            const url = urls[0]; // get the first URL if there is one overlap.
+            console.log(`Got Dev Request from: ${url.url}.`) // Log Successful request
+            const scripts_path = join(__dirname, "development"); // Get the path to development files
+
+            // Get files within the development folder.
+            // Than Filter the files with route context.
             const files = readdirSync(scripts_path).filter(file => file === `${url.context}.ts`);
+
             if (files.length < 1) {
+                // Log Error if no such file exists.
                 console.warn(chalk.red(`No such file as: "${scripts_path} + ${url.context}" for url: "${connectionReq}" matching "${url.url}"`));
-                break;
+                break; // End Request.
             }
             files.forEach(file => {
-                console.log(`Running "${file}"...`)
+                console.log(`Running "${file}"...`) // Log run attempt.
                 try {
-                    require(`${scripts_path}\\${file}`).main(req, res);
-                    console.log(chalk.green(`"${file}" Ran and Returned Successfully.`));
+                    require(`${scripts_path}\\${file}`).main(req, res); // Try and run file.
+                    console.log(chalk.green(`"${file}" Ran and Returned Successfully.`)); // Log Success.
                 } catch (e) {
-                    console.error(chalk.red(e))
+                    console.error(chalk.red(e)) // Error if something breaks.
                 }
             });
 
-            break;
+            break; // End Request
         default:
-            res.sendStatus(500);
-            console.warn(chalk.red(`Multiple routes for: ${connectionReq}.\nAll Routes:\n${urls}`));
-            break;
+            res.sendStatus(500); // Send Status 500 if there are too many routes with that name.
+            console.warn(chalk.red(`Multiple routes for: ${connectionReq}.\nAll Routes:\n${urls}`)); // Log Error
+            break; // End Request
     }
 }
 
+/**
+ * Checks if the request is a development request (API).
+ * @param connectionReq Hostname
+ * @returns boolean based on whether the hostname is a development request
+ */
 function isDevelopment(connectionReq: any): boolean {
     if (dev_routes.filter(route => route.url.startsWith(connectionReq)).length > 0) return true
     else return false;
 }
 
 app.use((req, res, next) => {
-	const connectionReq = req.headers.host;
-    if (isDevelopment(connectionReq)) return development_route(connectionReq, req, res);
-	if (connectionReq == undefined) return res.sendStatus(400)
-	console.log(`Request from: ${req.headers.host}.`);
-    main(connectionReq, req, res);
+	const connectionReq = req.headers.host; // Get Hostname from request
+	if (connectionReq == undefined) return res.sendStatus(400) // if there is no connection Request, Send Status 400
+    if (isDevelopment(connectionReq)) return development_route(connectionReq, req, res); // Check if its a Development Route.
+	console.log(`Request from: ${req.headers.host}.`); // Log the request attempt
+    main(connectionReq, req, res); // Call main function.
 });
 
+// Start Express App on port 80
 app.listen(80, () => {
-    console.clear()
-    console.log(chalk.cyan("Web Proxies:"))
+    console.clear() // Clear Console
+    console.log(chalk.cyan("Web Proxies:")) // Log Header for WebServers
     routes.forEach((route, i) => {
-        console.log(`${i}. From: ${route.url}, to ${route.route}.`)
+        console.log(`${i}. From: ${route.url}, to ${route.route}.`) // Log each route and its information
     });
-    console.log(chalk.cyan("\nDevelopment Proxies:"))
+    console.log(chalk.cyan("\nDevelopment Proxies:")) // Log Header for Development Files
     dev_routes.forEach((route, i) => {
-        console.log(`${i}. From: ${route.url}, to ${route.context}.`);
+        console.log(`${i}. From: ${route.url}, to ${route.context}.`); // Log each route and its information
     });
-	console.log(chalk.bgCyan("\nListening on port 80.\n"));
-    console.log("Logs")
-    const line = '-'.repeat(process.stdout.columns)
-    console.log(line)
-})
+	console.log(chalk.bgCyan("\nListening on port 80.\n")); // Log the port the app is listening on.
+    console.log("Logs") // App Log Header
+    const line = '-'.repeat(process.stdout.columns) // Create Separator line.
+    console.log(line); // Log Separator Line
+});
--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,11 @@
+// Route Object Layout
 export interface route {
-    url: string,
-    route: string
+    url: string // URL must be of type "string"
+    route: string // route must be of type "string"
 }
 
+// Development Route Object Layout
 export interface dev_route {
-    url: string,
-    context: string
+    url: string, // URL must be of type "string"
+    context: string // context must be of type "string"
 }
