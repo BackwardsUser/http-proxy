60513422b5b987dd3616d616383e7a9fe584dd07
Alexander Harding <AlexHardingYT@gmail.com>
2025-07-15 16:13:20 -0400

    Removed obnoxious per-line comments

--- src/types.ts
+++ src/types.ts
@@ -2,17 +2,17 @@ import {type Request, type Response} from "express";
 
 // Route Object Layout
 export type Route = {
-	url: string; // URL must be of type "string"
-	route: string; // Route must be of type "string"
+	url: string;
+	route: string;
 };
 
 // Built-in Route Object Layout
 export type BuiltInRoute = {
-	url: string; // URL must be of type "string"
-	context: string; // Context must be of type "string"
+	url: string;
+	context: string;
 };
 
 // Built-in Script File Layout
 export type BuiltInScript = {
-	main: (req: Request, res: Response) => void; // "main" function with no callback given "req" and "res"
+	main: (req: Request, res: Response) => void;
 };
